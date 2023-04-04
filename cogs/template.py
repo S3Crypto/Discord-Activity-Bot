6d942076ccbfc9ae1494e1def05453e2d68cb579
Mike <mike.s3crypto@gmail.com>
2023-04-04 22:45:30 +0100

    removed comments

--- cogs/template.py
+++ cogs/template.py
@@ -1,11 +1,3 @@
-""""
-Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
-
-Version: 5.5.0
-"""
-
 from discord.ext import commands
 from discord.ext.commands import Context
 
--- helpers/db_manager.py
+++ helpers/db_manager.py
@@ -1,13 +1,4 @@
-""""
-Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
-
-Version: 5.5.0
-"""
-
 import os
-
 import aiosqlite
 
 DATABASE_PATH = f"{os.path.realpath(os.path.dirname(__file__))}/../database/database.db"
